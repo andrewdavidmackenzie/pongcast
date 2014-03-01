942f33c8b13c23e5bd6fd8c58ac6da9691b37f62
Andrew Mackenzie <andrew@mackenzie-serres.net>
2014-03-01 11:57:54 +0100

    Separate out Game and Court

--- receiver/pong.js
+++ receiver/pong.js
@@ -1,31 +1,27 @@
 /**
  * Created by andrew on 24/02/14.
- *
- * See http://robots.thoughtbot.com/pong-clone-in-javascript
  */
 
 //////////////////////////////////// PADDLE ////////////////////////////////
-function Paddle(x, y, width, height, courtWidth, context) {
+function Paddle(x, y, width, height, courtWidth, color, context) {
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
     this.courtWidth = courtWidth;
+    this.color = color;
     this.context = context;
     this.x_speed = 0;
-    this.y_speed = 0;
 }
 
 Paddle.prototype.render = function () {
-    this.context.fillStyle = paddleColor;
+    this.context.fillStyle = this.color;
     this.context.fillRect(this.x, this.y, this.width, this.height);
 }
 
-Paddle.prototype.move = function (x, y) {
+Paddle.prototype.move = function (x) {
     this.x += x;
-    this.y += y;
     this.x_speed = x;
-    this.y_speed = y;
 
     if (this.x < 0) {
         this.x = 0;
@@ -39,9 +35,10 @@ Paddle.prototype.move = function (x, y) {
 //////////////////////////////////// BASE PLAYER ////////////////////////////////
 function BasePlayer(name) {
     this.name = name;
+    this.score = 0;
 }
 
-BasePlayer.prototype.givePaddle = function(paddle) {
+BasePlayer.prototype.givePaddle = function (paddle) {
     this.paddle = paddle;
 }
 
@@ -69,15 +66,16 @@ Computer.prototype.updatePaddle = function (ball) {
         diff = 5;
     }
 
-    this.paddle.move(diff, 0);
+    this.paddle.move(diff);
 }
 
 //////////////////////////////////// BALL ////////////////////////////////
-function Ball(x, y, context) {
+function Ball(x, y, ballSize, context) {
     this.width = x;
     this.height = y;
+    this.ballSize = ballSize;
     this.context = context;
-    this.reset(x/2, y/2);
+    this.reset(x / 2, y / 2);
 }
 
 // Reset the speed of the ball in X and Y to the default values
@@ -91,27 +89,27 @@ Ball.prototype.reset = function (x, y) {
 
 Ball.prototype.render = function () {
     this.context.beginPath();
-    this.context.arc(this.x, this.y, ballSize, 2 * Math.PI, false);
-    this.context.fillStyle = ballColor;
+    this.context.arc(this.x, this.y, this.ballSize, 2 * Math.PI, false);
+    this.context.fillStyle = this.ballColor;
     this.context.fill();
 }
 
 Ball.prototype.update = function (paddle1, paddle2) {
     this.x += this.x_speed;
     this.y += this.y_speed;
-    var top_x = this.x - ballSize;
-    var top_y = this.y - ballSize;
-    var bottom_x = this.x + ballSize;
-    var bottom_y = this.y + ballSize;
+    var top_x = this.x - this.ballSize;
+    var top_y = this.y - this.ballSize;
+    var bottom_x = this.x + this.ballSize;
+    var bottom_y = this.y + this.ballSize;
 
     // If hits the left wall
-    if (this.x - ballSize < 0) {
-        this.x = ballSize;
+    if (this.x - this.ballSize < 0) {
+        this.x = this.ballSize;
         this.x_speed = -this.x_speed;
     } else
     // if hits right wall
-    if (this.x + ballSize > this.width) {
-        this.x = this.width - ballSize;
+    if (this.x + this.ballSize > this.width) {
+        this.x = this.width - this.ballSize;
         this.x_speed = -this.x_speed;
     }
 
@@ -125,6 +123,7 @@ Ball.prototype.update = function (paddle1, paddle2) {
         this.reset(this.width / 2, this.height / 2);
     }
 
+    // Check for bounces of the paddles
     if (top_y > 300) {
         if (top_y < (paddle1.y + paddle1.height) && bottom_y > paddle1.y && top_x < (paddle1.x + paddle1.width) && bottom_x > paddle1.x) {
             this.y_speed = -3;
@@ -141,71 +140,97 @@ Ball.prototype.update = function (paddle1, paddle2) {
 }
 
 //////////////////////////////////// COURT ////////////////////////////////
-// TODO
+function Court(canvas) {
+    this.ballSize = 5;
+    this.ballColor = "#000000";
+    this.courtColor = "#FF00FF";
 
-//////////////////////////////////// GAME ////////////////////////////////
-function Game(canvas) {
-    window.game = this;
+    var fps = 60;
+    var paddleWidth = 50;
+    var paddleHeight = 10;
+    var paddleYOffset = 10;
+    var paddleColor = "#0000FF";
+
     this.width = canvas.width;
     this.height = canvas.height;
     this.context = canvas.getContext('2d');
-    this.paddle1 = new Paddle((this.width / 2) - (paddleWidth / 2), this.height - (paddleYOffset + paddleHeight), paddleWidth, paddleHeight, this.width, this.context);
-    this.paddle2 = new Paddle((this.width / 2) - (paddleWidth / 2), paddleYOffset, paddleWidth, paddleHeight, this.width, this.context);
+    this.paddle1 = new Paddle((this.width / 2) - (paddleWidth / 2), this.height - (paddleYOffset + paddleHeight), paddleWidth, paddleHeight, this.width, paddleColor, this.context);
+    this.paddle2 = new Paddle((this.width / 2) - (paddleWidth / 2), paddleYOffset, paddleWidth, paddleHeight, this.width, paddleColor, this.context);
+    // Create a new ball in the center of the court - not moving
+    this.ball = new Ball(this.width, this.height, this.ballSize, this.context);
 
     // Start off ready for demo mode with two computer players
     this.setPlayer1(new Computer("Demo1"));
     this.setPlayer2(new Computer("Demo2"));
+
+    // Install into the global window object
+    window.court = this;
 }
 
-Game.prototype.setPlayer1 = function(player) {
+Court.prototype.setPlayer1 = function (player) {
     this.player1 = player;
     this.player1.givePaddle(this.paddle1);
 }
 
-Game.prototype.setPlayer2 = function(computer) {
-    this.player2 = computer;
+Court.prototype.setPlayer2 = function (player) {
+    this.player2 = player;
     this.player2.givePaddle(this.paddle2);
+}
+
+Court.prototype.update = function () {
+    this.player2.updatePaddle(this.ball);
+    this.ball.update(this.paddle1, this.paddle2);
+}
+
+Court.prototype.clean = function () {
+    this.context.fillStyle = this.courtColor;
+    this.context.fillRect(0, 0, this.width, this.height);
+}
+
+Court.prototype.render = function () {
+    this.clean();
+    this.paddle1.render();
+    this.paddle2.render();
+    this.ball.render();
+    // TODO Render the score card
+}
+
+Court.prototype.step = function () {
+    // Update the positions of the objects
+    window.court.update();
+
+    // draw them
+    window.court.render();
+
+    // reschedule next animation update
+    window.animater(window.court.step);
+}
+
+//////////////////////////////////// GAME ////////////////////////////////
+function Game(court) {
+    this.court = court;
+    this.court.player1.score = 0;
+    this.court.player2.score = 0;
+    this.court.game = this;
+
+    // Create a new ball in the center of the court - Moving
+    this.court.ball = new Ball(this.court.width, this.court.height, this.court.ballSize, this.court.context);
 }
 
 // Only start the game if one or two players - second would be computer
 // Supply maximum number of poins for the game
 Game.prototype.start = function () {
-    this.player1.score = 0;
-    this.player2.score = 0;
-
-    // Create a new ball in the center of the court
-    this.ball = new Ball(this.width, this.height, this.context);
-
     // Start the updates
-    animate(window.game.step);
+    window.animater(window.court.step);
 }
 
-Game.prototype.step = function() {
-    // Update the positions of the objects
-    window.game.update();
-    // draw them
-    window.game.render();
-    // reschedule next animation update
-    animate(window.game.step);
+Game.prototype.point = function (player) {
 }
 
-Game.prototype.update = function () {
-    this.player2.updatePaddle(this.ball);
-    this.ball.update(this.paddle1, this.paddle2);
+Game.prototype.end = function () {
 }
 
-Game.prototype.cleanCourt = function () {
-    this.context.fillStyle = courtColor;
-    this.context.fillRect(0, 0, this.width, this.height);
-}
-
-Game.prototype.render = function () {
-    this.cleanCourt();
-    this.paddle1.render();
-    this.paddle2.render();
-    this.ball.render();
-}
-
-animate = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function (callback) {
+animater = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function (callback) {
     window.setTimeout(callback, 1000 / fps)
 };
+
