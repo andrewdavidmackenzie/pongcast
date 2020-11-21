48b381f8e55034b41e084403683e09fa8418184b
Andrew Mackenzie <andrew@mackenzie-serres.net>
2020-11-21 21:04:46 +0100

    Resurrection

---
+++ android/android/src/main/java/net/mackenzie_serres/chromecast/GameController.java
@@ -0,0 +1,19 @@
+package net.mackenzie_serres.chromecast;
+
+import androidx.annotation.NonNull;
+
+/**
+ * Interface definition that GameControllers must implement in order to be passed events and messages from
+ * ChromecastInteractor
+ * <p/>
+ * User: andrew
+ * Date: 25/01/15
+ * Time: 19:46
+ * <p/>
+ * Copyright Andrew Mackenzie, 2013
+ */
+public interface GameController {
+    void setChromecastInteractor(@NonNull final ChromecastInteractor chromecastInteractor);
+    void chromecastEvent(@NonNull final ChromecastInteractor.CHROMECAST_EVENT event);
+    void parseMessage(@NonNull final String message);
+}
---
+++ android/android/src/main/java/net/mackenzie_serres/pongcast/MainActivity.java
@@ -0,0 +1,71 @@
+package net.mackenzie_serres.pongcast;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.Menu;
+import net.mackenzie_serres.chromecast.ChromecastInteractor;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Main activity of the application
+ */
+public class MainActivity extends AppCompatActivity {
+    private PongController pongController;
+    private ChromecastInteractor chromecast;
+    private PongControllerView pongControllerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        pongController = new PongController();
+        pongControllerView = new PongControllerView(this, getString(R.string.app_id), pongController);
+        chromecast = new ChromecastInteractor(this, getString(R.string.app_id), getString(R.string.namespace),
+                pongControllerView.getMediaSelector(), pongController);
+    }
+
+    @Override
+    protected void onPause() {
+        pongController.pause();
+
+        // TODO try doing this always to make symmetrical with onResume()
+        if (isFinishing()) {
+            chromecast.pause();
+        }
+        super.onPause();
+    }
+
+    // TODO Avoid Pause/Resume on first rotation
+    @Override
+    protected void onResume() {
+        super.onResume();
+        chromecast.resume();
+    }
+
+    @Override
+    public void onDestroy() {
+        chromecast.disconnect();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        // Ignore configuration changes
+    }
+
+    /**
+     * Add the MediaRoute ("Chromecast") button to the ActionBar at the top of the app
+     *
+     * @param menu - menu to add the menu items to
+     * @return true if added an item
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main, menu);
+        pongControllerView.setMediaRouteSelector(menu);
+        return true;
+    }
+}
